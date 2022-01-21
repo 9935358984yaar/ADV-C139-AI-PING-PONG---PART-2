6cbd93f7be9364e7a9a7555aed85722b458dcc70
9935358984yaar <79708844+9935358984yaar@users.noreply.github.com>
2022-01-21 10:02:18 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+function setup(){
+    var canvas =  createCanvas(600,500);
+    canvas.position(270,120);
+    video=createCapture(VIDEO);
+    video.position(50, 50);
+    video.size(180, 200);
+}
